b15e93d1580dc81d48f31c767dc5c3bde5d3e04e
FaiqFdh <94748589+FaiqFdh@users.noreply.github.com>
2023-06-09 23:53:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ class RequestLoc(BaseModel):
     longitude: float
        
 @app.post("/predict_loc")
-#def recommend_locations(latitude: float = Query(...), longitude: float = Query(...), response: Response):
+#def recommend_locations(req:RequestLoc, response: Response):
 def recommend_locations(response: Response, latitude: float = Query(...), longitude: float = Query(...)):
     # Function implementation
 
